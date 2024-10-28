87ca2d9a501b4c62ccc9b7b8c95c9797ed09ae5a
darrly <1nguyenngocduy@gmail.com>
2024-10-28 08:37:54 +0700

    fix_error

--- router/index.js
+++ router/index.js
@@ -1,8 +1,8 @@
 const productRouter = require("./productRouter");
-
+const orderRouter = require("./orderRouter");
 const routes = (app) => {
   app.use("/api/product", productRouter);
-  app.use("/api");
+  app.use("/api", orderRouter);
 };
 
 module.exports = routes;
