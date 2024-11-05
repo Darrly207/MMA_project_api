db2dfcc282744e79d8cbad00dfaba26f66124e1b
darrly <1nguyenngocduy@gmail.com>
2024-11-06 00:35:55 +0700

    aaa

--- router/orderRouter.js
+++ router/orderRouter.js
@@ -5,9 +5,11 @@ const {
   getAllOrders,
   getOrderById,
 } = require("../controller/orderController"); // Adjust the path as necessary
-
+const { payment, callback } = require("../controller/paymentController");
 // Define routes
 router.post("/orders", createOrder);
 router.get("/orders", getAllOrders);
 router.get("/orders/:id", getOrderById);
+router.post("/payment", payment);
+router.post("/callback", callback);
 module.exports = router;
